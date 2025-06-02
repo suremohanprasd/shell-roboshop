dc8d15908b34a927b9eb461c8abab95817aa4231
mohan <mohanprasad390@gmail.com>
2025-06-02 23:54:30 +0530

    practice

--- 1-practice.sh
+++ 1-practice.sh
@@ -13,7 +13,7 @@ Y="-e \e[33m"
 N="\e[0m"
 
 LOGS_FOLDER="/var/log/shellscript-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." f1)    # $0 -> have the script name
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)    # $0 -> have the script name
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
