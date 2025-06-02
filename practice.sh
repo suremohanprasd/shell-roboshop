4a1111e313d57153a50a6391305b6b783fd43491
mohan <mohanprasad390@gmail.com>
2025-06-02 20:04:20 +0530

    practice

--- practice.sh
+++ practice.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-PERSON1=$1
-PERSON2=$2
+echo "Enter your pin"
 
-echo "$PERSON1  ::  HI $PERSON2, How are you?"
-echo "$PERSON2  ::  Hello $PERSON1 I am fine, What about you?"
-echo "$PERSON1  ::  I am also fine,Thanks for asking"
-echo "$PERSON2  ::  What are the plans for weekend"
-echo "$PERSON1  ::  Nothing much intrested, just sleeping and eating"
-echo "$PERSON2  ::  We are doing same thing every weeking, we need to plan better for this weekend"
-echo "$PERSON1  ::  Ok, plan $PERSON2 we will go outside."
+read PIN
+
+echo "your pin is : $PIN"
