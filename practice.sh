a1510a1aec51b08efbd447b162fa5477d71d17da
mohan <mohanprasad390@gmail.com>
2025-06-02 23:17:33 +0530

    practice

--- practice.sh
+++ practice.sh
@@ -7,57 +7,57 @@ USERID=$(id -u)
 PACKAGE1=mysql
 PACKAGE2=nginx
 PACKAGE3=httpd
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
+R="-e \e[31m"
+G="-e \e[32m"
+Y="-e \e[33m"
 N="\e[0m"
 
 VALIDATE() {
     if [ $1 -eq 0 ]
     then
-        echo "Installation of $2 is Success..."
+        echo $G "Installation of $2 is Success..." $N
     else
-        echo "Installation of $2 is Failed..."
+        echo $R "Installation of $2 is Failed..." $N
     exit 1
     fi
 }
 
 if [ $USERID -ne 0 ]
 then
-    echo -e $R "Please run with root access" $N
+    echo $R "Please run with root access" $N
 else
-    echo -e $G "You are running with root access" $N
+    echo $G "You are running with root access" $N
 fi
 
 dnf list installed $PACKAGE1 -y
 
 if [ $? -ne 0 ]
 then
-    echo "$PACKAGE1 is not installed please wait installing it..."
+    echo $R "$PACKAGE1 is not installed please wait installing it..." $N
     dnf install $PACKAGE1 -y
     VALIDATE $? "$PACKAGE1"
 else
-    echo "$PACKAGE1 is already installed"
+    echo $G "$PACKAGE1 is already installed" $N
 fi
 
 dnf list installed $PACKAGE2 -y
 
 if [ $? -ne 0 ]
 then
-    echo "$PACKAGE2 is not installed please wait installing it..."
+    echo $R "$PACKAGE2 is not installed please wait installing it..." $N
     dnf install $PACKAGE2 -y
     VALIDATE $? "$PACKAGE2"
 else
-    echo "$PACKAGE2 is already installed"
+    echo $G "$PACKAGE2 is already installed" $N
 fi
 
 dnf list installed $PACKAGE3 -y
 
 if [ $? -ne 0 ]
 then
-    echo "$PACKAGE3 is not installed please wait installing it..."
+    echo $R "$PACKAGE3 is not installed please wait installing it..." $N
     dnf install $PACKAGE3 -y
     VALIDATE $? "$PACKAGE3"
 else
-    echo "$PACKAGE3 is already installed"
+    echo $G "$PACKAGE3 is already installed" $N
 fi
