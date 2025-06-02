f79b26652143a695b7ea2e09e6af66fba985bc36
mohan <mohanprasad390@gmail.com>
2025-06-02 19:57:10 +0530

    practice

--- practice.sh
+++ practice.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "Iam Mohan Prasad Sure"
+echo "Mohan    ::  HI Sandeep, How are you?"
+echo "Sandeep  ::  Hello Mohan I am fine, What about you?"
+echo "Mohan    ::  I am also fine,Thanks for asking"
+echo "Sandeep  ::  What are the plans for weekend"
+echo "Mohan    ::  Nothing much intrested, just sleeping and eating"
+echo "Sandeep  ::  We are doing same thing every weeking, we need to plan better for this weekend"
+echo "Mohan    ::  Ok, plan sandeep we will go outside."
