443750210dbd79b9324bb7eba424cf7b384b1227
mohan <mohanprasad390@gmail.com>
2025-06-02 22:33:06 +0530

    practice

--- practice.sh
+++ practice.sh
@@ -8,13 +8,14 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "Please run with root access"
+    exit 1
 else
     echo "You are running with root access"
 fi
 
 dnf list installed mysql -y
 
-if [ $? -ne 0]
+if [ $? -ne 0 ]
 then
     echo "MySQL is not installed please wait installing it..."
     dnf install mysql -y
