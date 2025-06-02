57c71297fa4d26872bf49fbcf3cad90ad4d7dab0
mohan <mohanprasad390@gmail.com>
2025-06-03 00:31:47 +0530

    practice

---
+++ mongodb.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+R="-e \e[31m"
+G="-e \e[32m"
+Y="-e \e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)    # $0 -> have the script name
+mkdir -p $LOGS_FOLDER
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+PACKAGES=("mysql" "python3" "nginx" "httpd")
+
+
+echo "Script started executing at : $(date)" &>>$LOG_FILE
+
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo $R "ERROR:: Please run this script with root access" $N | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo $G "You are running with root access" $N | tee -a $LOG_FILE
+fi
+
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo $G "Installing $2 is ... SUCCESS" $N | tee -a $LOG_FILE
+    else
+        echo $R "Installing $2 is ... FAILURE" $N | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+cp $SCRIPT_DIR/mongodb.sh /etc/yum.repos.d/mongo.repo  &>>$LOG_FILE
+
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $1 "Installing Mongodb"
+
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $1 "Enabling Mongodb"
+
+systemctl start mongod  &>>$LOG_FILE
+VALIDATE $1 "Starting Mongodb"
+
+
+
--- practice2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo -e "\e[33m I am Mohan \e[0m"
-
-echo "I am from Vijayawad"
