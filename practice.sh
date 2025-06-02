5981b5b0b21063932285eee80d74cd16e3f4a50a
mohan <mohanprasad390@gmail.com>
2025-06-02 20:15:40 +0530

    practice

--- practice.sh
+++ practice.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-TIMESTAMP=$(time)
+TIMESTAMP=$(date)
+ENDTIME=$(date)
+TOTALTIME=$(($TIMESTAMP)-($ENDTIME))
 
 echo "Script run at the time of : $TIMESTAMP"
 
@@ -9,3 +11,7 @@ echo "Enter your pin"
 read -s PIN
 
 echo "your pin is : $PIN"
+
+echo "Script End at : $ENDTIME"
+
+echo "Total time to run the script : $TOTALTIME"
