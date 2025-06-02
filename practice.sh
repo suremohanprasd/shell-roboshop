487bdf8170abe79dcd7b2c602b2578c64b4f7599
mohan <mohanprasad390@gmail.com>
2025-06-02 21:56:23 +0530

    practice

--- practice.sh
+++ practice.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-NUMBER=$("$1" "$2" "$3" "$4")
+echo "Enter first number"
+read NUMBER1
+echo "Enter second number"
+read NUMBER2
+echo "Enter third number"
+read NUMBER3
+echo "Enter fouth number"
+read NUMBER4
+
+NUMBER=$("$NUMBER1" "$NUMBER2" "$NUMBER3" "$NUMBER4")
 
 if [ ${NUMBER[0]} lt 10]
 then
     echo "${NUMBER[0]} is less than 10"
 else
     echo "${NUMBER[0]} is greater than 10"
-fi 
+fi
