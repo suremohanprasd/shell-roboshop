a5c078e7c3265f958c55a6948d74dbf244de61e0
mohan <mohanprasad390@gmail.com>
2025-06-02 22:43:26 +0530

    practice

--- practice.sh
+++ practice.sh
@@ -28,3 +28,37 @@ then
 else
     echo "MySQL is already installed"
 fi
+
+dnf list installed nginx -y
+
+if [ $? -ne 0 ]
+then
+    echo "nginx is not installed please wait installing it..."
+    dnf install mysql -y
+    if [ $? -eq 0 ]
+    then
+        echo "Installation of Nginx is Success..."
+    else
+        echo "Installation of Nginx is Failed..."
+    exit 1
+    fi
+else
+    echo "Nginx is already installed"
+fi
+
+dnf list installed httd -y
+
+if [ $? -ne 0 ]
+then
+    echo "httd is not installed please wait installing it..."
+    dnf install mysql -y
+    if [ $? -eq 0 ]
+    then
+        echo "Installation of httd is Success..."
+    else
+        echo "Installation of httd is Failed..."
+    exit 1
+    fi
+else
+    echo "httd is already installed"
+fi
