03aea5e275fc4969240a44fadb75c93c8788b272
mohan <mohanprasad390@gmail.com>
2025-06-02 20:00:32 +0530

    practice

--- practice.sh
+++ practice.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-echo "Mohan    ::  HI Sandeep, How are you?"
-echo "Sandeep  ::  Hello Mohan I am fine, What about you?"
-echo "Mohan    ::  I am also fine,Thanks for asking"
-echo "Sandeep  ::  What are the plans for weekend"
-echo "Mohan    ::  Nothing much intrested, just sleeping and eating"
-echo "Sandeep  ::  We are doing same thing every weeking, we need to plan better for this weekend"
-echo "Mohan    ::  Ok, plan sandeep we will go outside."
+PERSON1=Sai
+PERSON2=Navya
+
+echo "$PERSON1  ::  HI $PERSON2, How are you?"
+echo "$PERSON2  ::  Hello $PERSON1 I am fine, What about you?"
+echo "$PERSON1  ::  I am also fine,Thanks for asking"
+echo "$PERSON2  ::  What are the plans for weekend"
+echo "$PERSON1  ::  Nothing much intrested, just sleeping and eating"
+echo "$PERSON2  ::  We are doing same thing every weeking, we need to plan better for this weekend"
+echo "$PERSON1  ::  Ok, plan $PERSON2 we will go outside."
